eecad88fa8f29355e00068267aef2e03d246b170
fluke <chaikhwan32@gmail.com>
2025-09-09 01:06:36 +0700

    style : add root variable

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -7,6 +7,8 @@ import Teacher from "./home/Teacher/Teacher";
 import Testimonial from "./home/Testimonial/Testimonial";
 import CTA from "./home/CTA/CTA";
 import Footer from "./home/Footer/Footer";
+// css file
+import "../css/Home/root.css"
 
 function Home(){
     return (
